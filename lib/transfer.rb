fe09dc3ed32a3e9c5da132247f68842f2ce09702
Melissa Real <melissa.real@hotmail.com>
2018-08-12 15:42:07 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,5 @@
 class Transfer
-  attr_accessor :bank_account, :status
+  attr_accessor :bankaccount, :status
   attr_reader :sender, :receiver, :amount
 
   def initialize(sender, receiver, amount)
