62fd700c4b0d9b3c6442f48a117ab518e29dcfc7
Melissa Real <melissa.real@hotmail.com>
2018-08-12 15:11:36 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,7 @@ class BankAccount
   attr_accessor :status, :balance
   attr_reader :name
 
-  def initialize(name, status='')
+  def initialize(name)
     @name = name
     @balance = 1000
     @status = 'open'
