e664148dc66c8d6fea6a18ec99fa08be0e855b17
Melissa Real <melissa.real@hotmail.com>
2018-08-12 15:34:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
 
   def valid?
-    BankAccount.valid?(self.sender) && BankAccount.valid?(self.reciever)
+    BankAccount.valid?(self.sender) && BankAccount.valid?(self.receiver)
   end
 
 
